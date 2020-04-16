db91cdc7edfed688edc6a2562d21092f2aa7474d
shun <shun.fix9@gmail.com>
2020-04-16 02:02:29 +0000

    add: ArrayStack

--- src/backend.rs
+++ src/backend.rs
@@ -1,73 +1,124 @@
+use std::error::Error;
+use std::fmt;
+
 #[derive(Debug)]
-pub(in crate) struct Array<'a, T> {
-    items: Vec<Entry<'a, T>>,
+pub(in crate) struct Array<T> {
+    items: Vec<Entry<T>>,
     length: usize,
 }
 
-impl<'a, T> Array<'a, T> {
-    pub(in crate) fn new(length: usize) -> Array<'a, T> {
+impl<T> Array<T> {
+    pub(in crate) fn new(length: usize) -> Array<T> {
         Array {
-            items: vec![Entry::default(); length],
-            length: length,
+            items: Self::new_items(length),
+            length,
         }
     }
 
-    pub(in crate) fn get(&self, index: usize) -> Option<Entry<'a, T>> {
+    fn new_items(length: usize) -> Vec<Entry<T>> {
+        let mut items = vec![];
+        let mut count = 0;
+        while count < length {
+            items.push(Entry::default());
+            count += 1;
+        }
+
+        items
+    }
+
+    pub(in crate) const fn len(&self) -> usize {
+        self.length
+    }
+
+    pub(in crate) fn get(&self, index: usize) -> Option<&Entry<T>> {
         if let Some(entry) = self.items.get(index) {
-            match entry {
-                Entry::Empty => Some(Entry::Empty),
-                Entry::Item(item) => Some(Entry::Item(item)),
-            }
+            Some(&entry)
         } else {
             None
         }
     }
 
-    pub(in crate) fn set(&mut self, index: usize, item: &'a T) -> Result<(), Error> {
+    pub(in crate) fn remove(&mut self, index: usize) -> Option<Entry<T>> {
+        if index < self.length {
+            let mut entry = Entry::default();
+            std::mem::swap(&mut entry, &mut self.items[index]);
+            Some(entry)
+        } else {
+            None
+        }
+    }
+
+    pub(in crate) fn set(&mut self, index: usize, item: T) -> Result<(), BackendError> {
         if index < self.length {
             self.items[index] = Entry::Item(item);
             Ok(())
         } else {
-            Err(OutOfBoundError)
+            Err(OUT_OF_BOUND_ERROR)
         }
     }
 
-    pub(in crate) fn drain<'b>(&mut self, other: Array<'b, T>)
-    where
-        'b: 'a,
-    {
-        self.items = other.items;
-        self.length = other.length;
+    pub(in crate) fn shift_right(&mut self, from: usize, to: usize) {
+        let mut target_index = to;
+        while target_index > from {
+            self.items.swap(target_index - 1, target_index);
+            target_index -= 1;
+        }
+    }
+
+    pub(in crate) fn shift_left(&mut self, from: usize, to: usize) {
+        let mut target_index = from;
+        while target_index < to {
+            self.items.swap(target_index, target_index + 1);
+            target_index += 1;
+        }
+    }
+
+    pub(in crate) fn resize(&mut self, from: usize, to: usize) {
+        let mut new_items = Self::new_items(to);
+
+        let mut index = 0;
+        while index < from {
+            std::mem::swap(&mut self.items[index], &mut new_items[index]);
+            index += 1;
+        }
+
+        self.items = new_items;
+        self.length = to;
     }
 }
 
 #[derive(Debug, PartialEq)]
-pub(in crate) enum Entry<'a, T> {
+pub(in crate) enum Entry<T> {
     Empty,
-    Item(&'a T),
+    Item(T),
 }
 
-impl<'a, T> Default for Entry<'a, T> {
+impl<T> Default for Entry<T> {
     fn default() -> Self {
         Self::Empty
     }
 }
 
-impl<'a, T> Clone for Entry<'a, T> {
-    fn clone(&self) -> Self {
-        match self {
-            Self::Empty => Self::Empty,
-            Self::Item(item) => Self::Item(item),
-        }
-    }
-}
-
 #[derive(Debug, PartialEq, Eq)]
-pub(in crate) struct Error {
+pub(in crate) struct BackendError {
     message: &'static str,
 }
 
-pub(in crate) const OutOfBoundError: Error = Error {
+impl BackendError {
+    pub(in crate) const fn message(&self) -> &'static str {
+        self.message
+    }
+}
+
+impl fmt::Display for BackendError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "error: {}", self.message)
+    }
+}
+
+impl Error for BackendError {}
+
+pub(in crate) const OUT_OF_BOUND_ERROR: BackendError = BackendError {
     message: "out of bound",
 };
 
@@ -79,16 +130,18 @@ mod tests {
     #[test]
     pub fn new_array() {
         let length = 5;
-        let array: Array<'static, i32> = Array::new(length);
+        let array: Array<i32> = Array::new(length);
+
+        assert_eq!(array.len(), length);
 
         check(
             &array,
             vec![
-                (0, Some(Entry::Empty)),
-                (1, Some(Entry::Empty)),
-                (2, Some(Entry::Empty)),
-                (3, Some(Entry::Empty)),
-                (4, Some(Entry::Empty)),
+                (0, Some(&Entry::Empty)),
+                (1, Some(&Entry::Empty)),
+                (2, Some(&Entry::Empty)),
+                (3, Some(&Entry::Empty)),
+                (4, Some(&Entry::Empty)),
                 (5, None),
             ],
         );
@@ -97,58 +150,135 @@ mod tests {
     #[test]
     pub fn set_values() {
         let length = 5;
-        let mut array: Array<'static, i32> = Array::new(length);
+        let mut array = Array::new(length);
 
-        assert_eq!(array.set(0, &1), Ok(()));
-        assert_eq!(array.set(1, &2), Ok(()));
-        assert_eq!(array.set(2, &3), Ok(()));
-        assert_eq!(array.set(3, &4), Ok(()));
-        assert_eq!(array.set(4, &5), Ok(()));
+        assert_eq!(array.set(0, 1), Ok(()));
+        assert_eq!(array.set(1, 2), Ok(()));
+        assert_eq!(array.set(2, 3), Ok(()));
+        assert_eq!(array.set(3, 4), Ok(()));
+        assert_eq!(array.set(4, 5), Ok(()));
 
-        assert_eq!(array.set(5, &6), Err(super::OutOfBoundError));
+        assert_eq!(array.set(5, 6), Err(super::OUT_OF_BOUND_ERROR));
 
         check(
             &array,
             vec![
-                (0, Some(Entry::Item(&1))),
-                (1, Some(Entry::Item(&2))),
-                (2, Some(Entry::Item(&3))),
-                (3, Some(Entry::Item(&4))),
-                (4, Some(Entry::Item(&5))),
+                (0, Some(&Entry::Item(1))),
+                (1, Some(&Entry::Item(2))),
+                (2, Some(&Entry::Item(3))),
+                (3, Some(&Entry::Item(4))),
+                (4, Some(&Entry::Item(5))),
                 (5, None),
             ],
         );
     }
 
     #[test]
-    pub fn drain() {
+    pub fn remove_values() {
         let length = 5;
-        let mut target: Array<'static, i32> = Array::new(length);
-        let mut array: Array<'static, i32> = Array::new(length);
+        let mut array = Array::new(length);
 
-        // set [target]
-        assert_eq!(target.set(0, &1), Ok(()));
-        assert_eq!(target.set(1, &2), Ok(()));
-        assert_eq!(target.set(2, &3), Ok(()));
-        assert_eq!(target.set(3, &4), Ok(()));
-        assert_eq!(target.set(4, &5), Ok(()));
+        assert_eq!(array.set(0, 1), Ok(()));
+        assert_eq!(array.set(1, 2), Ok(()));
+        assert_eq!(array.set(2, 3), Ok(()));
+        assert_eq!(array.set(3, 4), Ok(()));
+        assert_eq!(array.set(4, 5), Ok(()));
 
-        array.drain(target);
+        assert_eq!(array.set(5, 6), Err(super::OUT_OF_BOUND_ERROR));
+
+        assert_eq!(array.remove(2), Some(Entry::Item(3)));
 
         check(
             &array,
             vec![
-                (0, Some(Entry::Item(&1))),
-                (1, Some(Entry::Item(&2))),
-                (2, Some(Entry::Item(&3))),
-                (3, Some(Entry::Item(&4))),
-                (4, Some(Entry::Item(&5))),
+                (0, Some(&Entry::Item(1))),
+                (1, Some(&Entry::Item(2))),
+                (2, Some(&Entry::Empty)),
+                (3, Some(&Entry::Item(4))),
+                (4, Some(&Entry::Item(5))),
                 (5, None),
             ],
         );
     }
 
-    fn check<'a, T>(array: &Array<'a, T>, items: Vec<(usize, Option<Entry<'a, T>>)>)
+    #[test]
+    pub fn shift_right() {
+        let length = 5;
+        let mut array = Array::new(length);
+
+        assert_eq!(array.set(0, 1), Ok(()));
+        assert_eq!(array.set(1, 2), Ok(()));
+        assert_eq!(array.set(2, 3), Ok(()));
+
+        array.shift_right(2, 4);
+
+        check(
+            &array,
+            vec![
+                (0, Some(&Entry::Item(1))),
+                (1, Some(&Entry::Item(2))),
+                (2, Some(&Entry::Empty)),
+                (3, Some(&Entry::Item(3))),
+                (4, Some(&Entry::Empty)),
+                (5, None),
+            ],
+        );
+    }
+
+    #[test]
+    pub fn shift_left() {
+        let length = 5;
+        let mut array = Array::new(length);
+
+        assert_eq!(array.set(0, 1), Ok(()));
+        assert_eq!(array.set(1, 2), Ok(()));
+        assert_eq!(array.set(3, 3), Ok(()));
+
+        array.shift_left(2, 4);
+
+        check(
+            &array,
+            vec![
+                (0, Some(&Entry::Item(1))),
+                (1, Some(&Entry::Item(2))),
+                (2, Some(&Entry::Item(3))),
+                (3, Some(&Entry::Empty)),
+                (4, Some(&Entry::Empty)),
+                (5, None),
+            ],
+        );
+    }
+
+    #[test]
+    pub fn resize() {
+        let length = 3;
+        let mut array = Array::new(length);
+
+        assert_eq!(array.len(), length);
+
+        assert_eq!(array.set(0, 1), Ok(()));
+        assert_eq!(array.set(1, 2), Ok(()));
+        assert_eq!(array.set(2, 3), Ok(()));
+
+        let from = 2;
+        let to = 4;
+        array.resize(from, to);
+
+        assert_eq!(array.len(), to);
+
+        check(
+            &array,
+            vec![
+                (0, Some(&Entry::Item(1))),
+                (1, Some(&Entry::Item(2))),
+                (2, Some(&Entry::Empty)),
+                (3, Some(&Entry::Empty)),
+                (4, None),
+            ],
+        );
+    }
+
+    fn check<T>(array: &Array<T>, items: Vec<(usize, Option<&Entry<T>>)>)
     where
         T: std::fmt::Debug + std::cmp::PartialEq,
     {
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use open_data_structures::array_stack;
+use open_data_structures::Stack;
+
 fn main() {
     println!("Hello, world!");
+    array_stack();
+}
+
+fn array_stack() {
+    let mut array = array_stack::ArrayStack::new();
+
+    array.push(42);
+
+    println!("{:?}", array);
 }
